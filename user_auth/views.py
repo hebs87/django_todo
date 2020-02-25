d8573798116aef261b48d61bc0eba1055850167f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-25 22:38:41 +0000

    Amended register view to create new user object if passwords match

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,12 +1,31 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def register(request):
-
-    context = {
-        'register_form': UserCreationForm
-    }
-
-    return render(request, 'register.html', context)
+    """
+    A view that allows users to register to the site.
+    If the user routes to the register page, the register
+    form is displayed. If they submit the form, a new user
+    is created
+    """
+    # When routed to the register page
+    if request.method == 'GET':
+        context = {
+            'register_form': UserCreationForm
+        }
+        return render(request, 'register.html', context)
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # Create new user
+            user = User.objects.create_user(
+                request.POST['username'],
+                password=request.POST['password1']
+            )
+            # Insert the user object into the database
+            user.save()
+        else:
+            # Tell user the passwords didn't match
+            print("Passwords don't match")
