e06a10c435f7325bf43d1fe6b43e8ec8597d54c0
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-25 21:30:22 +0000

    Created basic registration form and register url and view

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo.apps.TodoConfig',
+    'user_auth.apps.UserAuthConfig',
 ]
 
 MIDDLEWARE = [
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def register(request):
+
+    context = {
+        'register_form': UserCreationForm
+    }
+
+    return render(request, 'register.html', context)
