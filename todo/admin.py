5059b6e9259ae915423f74fca1a58db543c6daef
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-28 22:34:33 +0000

    Created Todo model and registered it in admin site

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    """
+    A class to make the date_created field a viewable
+    read-only field in the admin site
+    """
+    readonly_fields = ('date_created',)
+
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    # Optional field - blank by default
+    memo = models.TextField(blank=True)
+    # Created date will be automatically set; non-editable
+    date_created = models.DateTimeField(auto_now_add=True)
+    # Value is null by default as it will be updated when marked as completed
+    # Set blank=True to enable leaving field blank in admin site
+    date_completed = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    # Sets the user as the author using the user's ID as the ForeignKey
+    # This enables us to filter out the tasks created by a particular user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.user.username, self.title)
